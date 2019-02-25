9da06459068faac5aa3e4e6dcf47ece87c04e80d
mattorus <mattorus@gmail.com>
2019-02-25 14:23:07 -0800

    Update session controller & routes; enable login functionality in UI.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,25 @@ class SessionsController < ApplicationController
 
   end
 
+  def create
+    # byebug
+    user = User.find_by(username: params[:session][:username])
+    if (user && user.authenticate(params[:session][:password]))
+      # good scenario
+      session[:user_id] = user.id
+      flash[:success] = "Login succeeded"
+      redirect_to root_path
+
+    else
+      flash.now[:error] = "Login failed"
+      render 'new'
+    end    
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Logout succeeded"
+    redirect_to login_path
+  end
+
 end
